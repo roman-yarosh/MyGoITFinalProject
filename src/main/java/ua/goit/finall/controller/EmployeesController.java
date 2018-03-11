64da93a380c95e19e83a8c58fa99e5d1893f4927
Roman Yarosh <yracompany@gmail.com>
2018-03-11 21:20:16 +0200

    EmployeesRestController Make Rest Controller and added crud in json (RY)

--- src/main/java/ua/goit/finall/controller/EmployeesController.java
+++ src/main/java/ua/goit/finall/controller/EmployeesController.java
@@ -3,12 +3,10 @@ package ua.goit.finall.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import ua.goit.finall.model.Employee;
 import ua.goit.finall.service.EmployeeService;
 
@@ -22,7 +20,7 @@ public class EmployeesController {
     @Autowired
     private EmployeeService employeeService;
 
-    @RequestMapping(value = "/employeeInfo", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @RequestMapping("/employeeInfo")
     public String employeeInfo(Map<String, Object> model) {
         String userName = getLoggedinUserName();
         Employee employee = employeeService.findEmployeeByName(userName);
