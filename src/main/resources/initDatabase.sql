6017070bbfb572430566cef5c4eb4bd9d93f46c9
Roman Yarosh <yracompany@gmail.com>
2018-03-11 14:49:17 +0200

    Corrected initDatabase.sql  (RY)

--- src/main/resources/initDatabase.sql
+++ src/main/resources/initDatabase.sql
@@ -21,24 +21,22 @@ USE goit_final_project;
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS employees;
 CREATE TABLE employees (
-  EMPLOYEE_ID     INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
---  LOGIN           VARCHAR(20)      NOT NULL,
---  PASSWORD        VARCHAR(20)      NOT NULL,
+  ID              INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   NAME            VARCHAR(20)      NOT NULL,
   SURNAME         VARCHAR(20)      NOT NULL,
   MIDDLE_NAME     VARCHAR(20),
   EMAIL           VARCHAR(50)      NOT NULL,
---  SALLARY_RATE    DECIMAL          NOT NULL,
---  ROLE_ID         INT              NOT NULL,
+  REGISTERED_DATE DATETIME         NOT NULL,
   POSITION_ID     INT(10)          NOT NULL,
   DEPARTMENT_ID   INT(10)          NOT NULL,
---  EVENT_ID        INT,
-  STATUS_ID       INT(10)           NOT NULL,
-  REGISTERED_DATE DATETIME         NOT NULL,
+  STATUS_ID       INT(10)          NOT NULL,
   USER_ID         INT(10)          NOT NULL,
 
-  PRIMARY KEY (EMPLOYEE_ID) USING BTREE,
---  UNIQUE KEY UNI_LOGIN (LOGIN),
+  PRIMARY KEY (ID) USING BTREE,
+  FOREIGN KEY (POSITION_ID) REFERENCES positions (POSITION_ID),
+  FOREIGN KEY (DEPARTMENT_ID) REFERENCES departments (DEPARTMENT_ID),
+  FOREIGN KEY (STATUS_ID) REFERENCES statuses (STATUS_ID),
+  FOREIGN KEY (USER_ID) REFERENCES users (USER_ID),
   UNIQUE KEY UNI_EMAIL (EMAIL)
 )
   ENGINE = InnoDB
@@ -50,10 +48,10 @@ CREATE TABLE employees (
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS roles;
 CREATE TABLE roles (
-  ROLE_ID INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  NAME    VARCHAR(255)     NOT NULL,
+  ID   INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  NAME VARCHAR(255)     NOT NULL,
 
-  PRIMARY KEY (ROLE_ID) USING BTREE,
+  PRIMARY KEY (ID) USING BTREE,
   UNIQUE KEY UNI_NAME (NAME)
 )
   ENGINE = InnoDB
@@ -65,11 +63,11 @@ CREATE TABLE roles (
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-  USER_ID  INT(10)      NOT NULL AUTO_INCREMENT,
+  ID       INT(10)      NOT NULL AUTO_INCREMENT,
   USERNAME VARCHAR(255) NOT NULL,
   PASSWORD VARCHAR(255) NOT NULL,
 
-  PRIMARY KEY (USER_ID) USING BTREE,
+  PRIMARY KEY (ID) USING BTREE,
   UNIQUE KEY UNI_USERNAME (USERNAME)
 )
   ENGINE = InnoDB
@@ -81,10 +79,10 @@ CREATE TABLE users (
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS departments;
 CREATE TABLE departments (
-  DEPARTMENT_ID INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  NAME          VARCHAR(128)     NOT NULL,
+  ID   INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  NAME VARCHAR(128)     NOT NULL,
 
-  PRIMARY KEY (DEPARTMENT_ID) USING BTREE,
+  PRIMARY KEY (ID) USING BTREE,
   UNIQUE KEY UNI_NAME (NAME)
 )
   ENGINE = InnoDB
@@ -96,10 +94,11 @@ CREATE TABLE departments (
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS positions;
 CREATE TABLE positions (
-  POSITION_ID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  ID          INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   NAME        VARCHAR(128) NOT NULL,
+  HOUR_SALARY DECIMAL NOT NULL,
 
-  PRIMARY KEY (POSITION_ID) USING BTREE,
+  PRIMARY KEY (ID) USING BTREE,
   UNIQUE KEY UNI_NAME (NAME)
 )
   ENGINE = InnoDB
@@ -111,14 +110,13 @@ CREATE TABLE positions (
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS events;
 CREATE TABLE events (
-  EVENT_ID            INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  NAME                VARCHAR(128)     NOT NULL,
-  TYPE_ID             INT(10)          NOT NULL,
-  USER_ID             INT(10)          NOT NULL,
-  EVENT_DATE          DATETIME         NOT NULL,
-  HOURS               INT(10)          NOT NULL,
+  ID         INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  EVENT_DATE DATETIME         NOT NULL,
+  HOURS      INT(10)          NOT NULL,
+  TYPE_ID    INT(10)          NOT NULL,
+  USER_ID    INT(10)          NOT NULL,
 
-  PRIMARY KEY (EVENT_ID) USING BTREE,
+  PRIMARY KEY (ID) USING BTREE,
   FOREIGN KEY (TYPE_ID) REFERENCES event_types (TYPE_ID),
   FOREIGN KEY (USER_ID) REFERENCES users (USER_ID)
 )
@@ -131,11 +129,11 @@ CREATE TABLE events (
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS event_types;
 CREATE TABLE event_types (
-  TYPE_ID            INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  TYPE               VARCHAR(128)     NOT NULL,
-  RATE               DECIMAL          NOT NULL,
+  ID   INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  TYPE VARCHAR(128)     NOT NULL,
+  RATE DECIMAL          NOT NULL,
 
-  PRIMARY KEY (TYPE_ID) USING BTREE
+  PRIMARY KEY (ID) USING BTREE
 )
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
@@ -146,34 +144,14 @@ CREATE TABLE event_types (
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS statuses;
 CREATE TABLE statuses (
-  STATUS_ID INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  TYPE      VARCHAR(10)      NOT NULL,
+  ID   INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  TYPE VARCHAR(10)      NOT NULL,
 
-  PRIMARY KEY (STATUS_ID) USING BTREE,
+  PRIMARY KEY (ID) USING BTREE,
   UNIQUE KEY UNI_TYPE (TYPE)
 )
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
-
--- -----------------------------------------------------
--- Table: employee_status
--- table for mapping employee and status.
--- !!! Statuses will be one to many not many to many, so this table is not need
--- -----------------------------------------------------
--- DROP TABLE IF EXISTS employee_status;
--- CREATE TABLE employee_status (
---  EMPLOYEE_ID    INT(10) UNSIGNED NOT NULL,
---  STATUS_ID      INT(10) UNSIGNED NOT NULL,
---  START_DATETIME DATETIME         NOT NULL,
---  END_DATETIME   DATETIME         NOT NULL,
-
---  FOREIGN KEY (EMPLOYEE_ID) REFERENCES employees (EMPLOYEE_ID),
---  FOREIGN KEY (STATUS_ID) REFERENCES statuses (STATUS_ID),
-
---  UNIQUE (STATUS_ID, EMPLOYEE_ID)
--- )
---  ENGINE = InnoDB
---  DEFAULT CHARSET = utf8;
 
 -- -----------------------------------------------------
 -- Table: employee_events
@@ -199,8 +177,8 @@ CREATE TABLE employee_events (
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS user_roles;
 CREATE TABLE user_roles (
-  USER_ID     INT(10) UNSIGNED NOT NULL,
-  ROLE_ID     INT(10) UNSIGNED NOT NULL,
+  USER_ID INT(10) UNSIGNED NOT NULL,
+  ROLE_ID INT(10) UNSIGNED NOT NULL,
 
   FOREIGN KEY (USER_ID) REFERENCES users (USER_ID),
   FOREIGN KEY (ROLE_ID) REFERENCES roles (ROLE_ID),
@@ -211,50 +189,18 @@ CREATE TABLE user_roles (
   DEFAULT CHARSET = utf8;
 
 -- -----------------------------------------------------
--- Table: employee_departments
--- table for mapping employee and department.
--- -----------------------------------------------------
-DROP TABLE IF EXISTS employee_department;
-CREATE TABLE employee_department (
-  EMPLOYEE_ID   INT(10) UNSIGNED NOT NULL,
-  DEPARTMENT_ID INT(10) UNSIGNED NOT NULL,
-
-  FOREIGN KEY (EMPLOYEE_ID) REFERENCES employees (EMPLOYEE_ID),
-  FOREIGN KEY (DEPARTMENT_ID) REFERENCES departments (DEPARTMENT_ID),
-
-  UNIQUE (EMPLOYEE_ID, DEPARTMENT_ID)
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
-
--- -----------------------------------------------------
--- Table: employee_departments
--- table for mapping employee and position.
--- -----------------------------------------------------
-DROP TABLE IF EXISTS employee_position;
-CREATE TABLE employee_position (
-  EMPLOYEE_ID INT(10) UNSIGNED NOT NULL,
-  POSITION_ID INT(10) UNSIGNED NOT NULL,
-
-  FOREIGN KEY (EMPLOYEE_ID) REFERENCES employees (EMPLOYEE_ID),
-  FOREIGN KEY (POSITION_ID) REFERENCES positions (POSITION_ID),
-
-  UNIQUE (EMPLOYEE_ID, POSITION_ID)
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
-
--- -----------------------------------------------------
 -- Table: salaries
 -- table for employee salaries for each month
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS salaries;
 CREATE TABLE salaries (
-  EMPLOYEE_ID INT(10) UNSIGNED NOT NULL,
+  ID          INT(10) UNSIGNED NOT NULL,
   MONTH       INT              NOT NULL,
   YAER        INT              NOT NULL,
---  SALARY_DATE DATE             NOT NULL,
   SALARY_SUM  DECIMAL          NOT NULL,
+  EMPLOYEE_ID INT(10) UNSIGNED NOT NULL,
 
+  PRIMARY KEY (ID) USING BTREE,
   FOREIGN KEY (EMPLOYEE_ID) REFERENCES employees (EMPLOYEE_ID)
 )
   ENGINE = InnoDB
