a79cca0bdd8b461d69cfd0d95e0a3fbbf663f1fd
Roman Yarosh <yracompany@gmail.com>
2018-03-11 21:18:51 +0200

    EmployeesRestController Make Rest Controller and added crud in json (RY)

--- src/main/java/ua/goit/finall/controller/EmployeesController.java
+++ src/main/java/ua/goit/finall/controller/EmployeesController.java
@@ -1,26 +1,20 @@
 package ua.goit.finall.controller;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import ua.goit.finall.model.Employee;
 import ua.goit.finall.service.EmployeeService;
 
-import javax.validation.Valid;
-import java.util.List;
 import java.util.Map;
 
-@RestController
-@RequestMapping(value = "api/employees")
+@Controller
 public class EmployeesController {
 
     private Logger logger = LoggerFactory.getLogger(EmployeesController.class.getName());
@@ -49,58 +43,5 @@ public class EmployeesController {
 
         return principal.toString();
     }
-
-    @RequestMapping(value = "", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<List<Employee>> getAllEmployees() {
-        List<Employee> employeeList = this.employeeService.getAll();
-
-        if (employeeList.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-
-        return new ResponseEntity<>(employeeList, HttpStatus.OK);
-    }
-
-    @RequestMapping(value = "/{employeeId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<Employee> getEmployee(@PathVariable("employeeId") Long employeeId) {
-        if (employeeId == null) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-
-        Employee employee = this.employeeService.getById(employeeId);
-
-        if (employee == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-
-        return new ResponseEntity<>(employee, HttpStatus.OK);
-    }
-
-    @RequestMapping(value = "", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<Employee> saveEmployee(@RequestBody @Valid Employee employee, UriComponentsBuilder builder) {
-        HttpHeaders headers = new HttpHeaders();
-
-        if (employee == null) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-
-        this.employeeService.save(employee);
-        headers.setLocation(builder.path("/api/employees/{employeeId}").buildAndExpand(employee.getId()).toUri());
-        return new ResponseEntity<>(employee, headers, HttpStatus.CREATED);
-    }
-
-    @RequestMapping(value = "/{employeeId}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<Employee> deleteEmployee(@PathVariable("employeeId") Long employeeId) {
-        Employee employee = this.employeeService.getById(employeeId);
-
-        if (employee == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-
-        this.employeeService.delete(employeeId);
-
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
-
 
 }
